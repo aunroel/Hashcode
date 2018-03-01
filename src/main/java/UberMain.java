d4e2fee51daccfd59cc820747e8ccb8c985865e8
Roman Matios <rm252@st-andrews.ac.uk>
2018-03-01 18:46:55 +0000

    Works

--- src/main/java/UberMain.java
+++ src/main/java/UberMain.java
@@ -3,7 +3,8 @@ public class UberMain {
     public static void main(String[] args) {
         FileReader fr = new FileReader();
 
-        fr.readFile("a_example.in");
+//        fr.readFile("a_example.in");
+        fr.readFile("b_should_be_easy.in");
         fr.parseData();
     }
 }
